6b0d8ca034634671a7c275d6e9b336e8caf69ea7
Shinjini123 <92155085+Shinjini123@users.noreply.github.com>
2023-06-09 12:52:32 +0530

    changes made

--- frontend-flipr/src/App.js
+++ frontend-flipr/src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Login from './pages/Login';
-import AddEmployee from './pages/Employee_login';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './pages/login';
 import Employee_login from './pages/Employee_login';
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Login} />
-        <Route path="/add-employee" component={Employee_login} />
-      </Switch>
+      <Routes>
+        <Route exact path="/" component={<Login/>} />
+        <Route path="/add-employee" component={<Employee_login/>} />
+      </Routes>
     </Router>
   );
 };
